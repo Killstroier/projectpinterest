ae4689dcef4ce2c3c2a86bbbb236205311e39aee
Aleksandr Matveev <aleksmatveev19@mail.ru>
2025-03-21 21:06:59 +0700

    шаманим над urls.py

--- projectpinterest/board/urls.py
+++ projectpinterest/board/urls.py
@@ -7,9 +7,9 @@ from .converters import FourDigitYearConverter
 register_converter(FourDigitYearConverter, 'yyyy')
 
 urlpatterns = [
+    path('', views.index, name='home'),
     path('archive/<yyyy:year>/', views.archive, name='archive'),
-    path('photo/<int:photo_id>/', views.photo_detail, name='photo_detail'),
+    path('post/<slug:post_slug>/', views.show_post, name='post'),
     path('create/', views.photo_create, name='photo_create'),
-    path('redirect/', views.redirect_example, name='redirect_example'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
 ]
