05c80d763f089194e537398bc77940ca02455353
Aleksandr Matveev <aleksmatveev19@mail.ru>
2025-03-21 21:31:44 +0700

    доабвил кнопки удалить и редактировать

--- projectpinterest/board/urls.py
+++ projectpinterest/board/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('post/<slug:post_slug>/', views.show_post, name='post'),
     path('create/', views.photo_create, name='photo_create'),
     path('about/', views.about, name='about'),
+    path('edit/<slug:post_slug>/', views.photo_edit, name='photo_edit'),
+    path('delete/<slug:post_slug>/', views.photo_delete, name='photo_delete'),
 ]
--- projectpinterest/board/views.py
+++ projectpinterest/board/views.py
@@ -2,14 +2,25 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Photo
 from .forms import PhotoForm
 
+
 def index(request):
-    # Используем менеджер published для выбора только опубликованных постов
-    posts = Photo.published.all()
+    filter_status = request.GET.get('filter')
+    if filter_status == 'draft':
+        posts = Photo.objects.filter(is_published=Photo.Status.DRAFT)
+    elif filter_status == 'published':
+        posts = Photo.published.all()
+    elif filter_status == 'all':
+        posts = Photo.objects.all()
+    else:
+        # По умолчанию показываем только опубликованные
+        posts = Photo.published.all()
+
     data = {
         'title': 'Главная страница',
         'posts': posts,
     }
     return render(request, 'board/index.html', data)
+
 
 def show_post(request, post_slug):
     # Получаем пост по слагу; если не найден – генерируется 404
@@ -19,6 +30,7 @@ def show_post(request, post_slug):
         'post': post,
     }
     return render(request, 'board/post.html', data)
+
 
 def photo_create(request):
     if request.method == 'POST':
@@ -37,6 +49,27 @@ def photo_create(request):
     else:
         form = PhotoForm()
     return render(request, 'board/photo_create.html', {'form': form})
+
+
+def photo_edit(request, post_slug):
+    photo = get_object_or_404(Photo, slug=post_slug)
+    if request.method == 'POST':
+        form = PhotoForm(request.POST, request.FILES, instance=photo)
+        if form.is_valid():
+            edited_photo = form.save()
+            return redirect(edited_photo.get_absolute_url())
+    else:
+        form = PhotoForm(instance=photo)
+    return render(request, 'board/photo_edit.html', {'form': form, 'photo': photo})
+
+
+def photo_delete(request, post_slug):
+    photo = get_object_or_404(Photo, slug=post_slug)
+    if request.method == 'POST':
+        photo.delete()
+        return redirect('home')  # После удаления возвращаемся на главную
+    return render(request, 'board/photo_delete_confirm.html', {'photo': photo})
+
 
 def about(request):
     data = {
