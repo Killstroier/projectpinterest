642055a8acfd604be76493e3617b9e369d9853ef
Aleksandr Matveev <aleksmatveev19@mail.ru>
2025-03-21 20:58:50 +0700

    обновлены urls

--- projectpinterest/projectpinterest/urls.py
+++ projectpinterest/projectpinterest/urls.py
@@ -1,26 +1,13 @@
-"""
-URL configuration for projectpinterest project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-from board import views as board_views
+from django.conf import settings
+from django.conf.urls.static import static
 
-handler404 = 'board.views.page_not_found'
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', board_views.home, name='home'),  # Домашняя страница
-    path('', include('board.urls')),  # Подключение маршрутов приложения board
+    path('', include('board.urls')),  # Подключаем маршруты из приложения board
 ]
+
+# Для работы медиафайлов (изображений) при отладке:
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
