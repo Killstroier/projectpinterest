1bab9d9ba094c917ccb9511a6843afdc249f199e
Aleksandr Matveev <aleksmatveev19@mail.ru>
2025-03-21 21:04:53 +0700

    и это тоже

--- projectpinterest/board/forms.py
+++ projectpinterest/board/forms.py
@@ -4,4 +4,4 @@ from .models import Photo
 class PhotoForm(forms.ModelForm):
     class Meta:
         model = Photo
-        fields = ['title', 'slug', 'description', 'image', 'is_published']
+        fields = ['Заголовок', 'слаг', 'Описание', 'Изображение', 'Опубликован']
