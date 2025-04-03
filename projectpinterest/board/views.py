7b4955b9bb3548aed69e171088d9b3b65cbac4e9
Aleksandr Matveev <aleksmatveev19@mail.ru>
2025-04-03 19:43:49 +0700

    обновил views.py

--- projectpinterest/board/views.py
+++ projectpinterest/board/views.py
@@ -1,19 +1,18 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Photo
+from .models import Photo, Category, TagPost
 from .forms import PhotoForm
 
-
+# Главная страница с фильтром по статусу
 def index(request):
     filter_status = request.GET.get('filter')
     if filter_status == 'draft':
         posts = Photo.objects.filter(is_published=Photo.Status.DRAFT)
     elif filter_status == 'published':
-        posts = Photo.published.all()
+        posts = Photo.objects.filter(is_published=Photo.Status.PUBLISHED)
     elif filter_status == 'all':
         posts = Photo.objects.all()
     else:
-        # По умолчанию показываем только опубликованные
-        posts = Photo.published.all()
+        posts = Photo.objects.filter(is_published=Photo.Status.PUBLISHED)
 
     data = {
         'title': 'Главная страница',
@@ -21,36 +20,12 @@ def index(request):
     }
     return render(request, 'board/index.html', data)
 
-
+# Страница для показа одного поста
 def show_post(request, post_slug):
-    # Получаем пост по слагу; если не найден – генерируется 404
     post = get_object_or_404(Photo, slug=post_slug)
-    data = {
-        'title': post.title,
-        'post': post,
-    }
-    return render(request, 'board/post.html', data)
+    return render(request, 'board/post.html', {'post': post})
 
-
-def photo_create(request):
-    if request.method == 'POST':
-        form = PhotoForm(request.POST, request.FILES)
-        if form.is_valid():
-            new_post = form.save(commit=False)
-            # Если слаг не задан, можно автогенерировать (например, на основе title)
-            if not new_post.slug:
-                from django.utils.text import slugify
-                new_post.slug = slugify(new_post.title)
-            new_post.save()
-            return redirect(new_post.get_absolute_url())
-        else:
-            # Вывод ошибок в консоль для отладки
-            print(form.errors)
-    else:
-        form = PhotoForm()
-    return render(request, 'board/photo_create.html', {'form': form})
-
-
+# Редактирование поста
 def photo_edit(request, post_slug):
     photo = get_object_or_404(Photo, slug=post_slug)
     if request.method == 'POST':
@@ -62,7 +37,7 @@ def photo_edit(request, post_slug):
         form = PhotoForm(instance=photo)
     return render(request, 'board/photo_edit.html', {'form': form, 'photo': photo})
 
-
+# Удаление поста
 def photo_delete(request, post_slug):
     photo = get_object_or_404(Photo, slug=post_slug)
     if request.method == 'POST':
@@ -70,9 +45,13 @@ def photo_delete(request, post_slug):
         return redirect('home')  # После удаления возвращаемся на главную
     return render(request, 'board/photo_delete_confirm.html', {'photo': photo})
 
-
-def about(request):
-    data = {
-        'title': 'О сайте',
-    }
-    return render(request, 'board/about.html', data)
+# Создание нового поста
+def photo_create(request):
+    if request.method == 'POST':
+        form = PhotoForm(request.POST, request.FILES)
+        if form.is_valid():
+            new_photo = form.save()
+            return redirect(new_photo.get_absolute_url())
+    else:
+        form = PhotoForm()
+    return render(request, 'board/photo_create.html', {'form': form})
