f936cf9585acb2f5798856425431e4ec7818fd15
Aleksandr Matveev <aleksmatveev19@mail.ru>
2025-04-03 19:44:35 +0700

    добавил форму для Photo

--- projectpinterest/board/forms.py
+++ projectpinterest/board/forms.py
@@ -1,7 +1,10 @@
 from django import forms
-from .models import Photo
+from .models import Photo, TagPost, Category
 
 class PhotoForm(forms.ModelForm):
+    tags = forms.ModelMultipleChoiceField(queryset=TagPost.objects.all(), required=False)
+    category = forms.ModelChoiceField(queryset=Category.objects.all())
+
     class Meta:
         model = Photo
-        fields = ['title', 'slug', 'description', 'image', 'is_published']
+        fields = ['title', 'description', 'image', 'category', 'tags']
